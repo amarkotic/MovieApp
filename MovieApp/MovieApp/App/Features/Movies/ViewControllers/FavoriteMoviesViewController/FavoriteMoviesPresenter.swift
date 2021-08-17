8ac3e23376e3c3030f57034290256c248d7e503a
srcxp <antoniomarkotic1@gmail.com>
2021-08-17 13:22:48 +0200

    Revert "favorites working"
    This reverts commit e6b715232a1d0723897658cedc86995865852ef2.

--- MovieApp/MovieApp/App/Features/Movies/ViewControllers/FavoriteMoviesViewController/FavoriteMoviesPresenter.swift
+++ MovieApp/MovieApp/App/Features/Movies/ViewControllers/FavoriteMoviesViewController/FavoriteMoviesPresenter.swift
@@ -1,7 +1,7 @@
 import UIKit
 
 class FavoriteMoviesPresenter {
-    
+
     private let moviesUseCase: MoviesUseCaseProtocol
     
     var data = [MovieViewModel]()
@@ -16,19 +16,25 @@ class FavoriteMoviesPresenter {
     }
     
     func fetchMovies() {
-        var temporaryArray = [MovieViewModel]()
-        moviesUseCase.favoriteItems.forEach { [weak self] id in
+        moviesUseCase.fetchMovies(categoryViewModel: .popular, subcategoryViewModel: .action) { [weak self]
+            (result: Result<[MovieModel], Error>) in
             guard let self = self else { return }
             
-            moviesUseCase.fetchMovie(with: id) { (result: Result<MovieDetailsModel, Error>) in
-                switch result {
-                case .failure(let error):
-                    print(error.localizedDescription)
-                case .success(let value):
-                    temporaryArray.append(MovieViewModel(id: id, imageUrl: value.posterPath, isFavorite: true))
-                    self.data = temporaryArray
-                    self.delegate?.reloadData()
+            switch result {
+            case .failure(let error):
+                print(error.localizedDescription)
+            case .success(let value):
+                let viewModels: [MovieViewModel] = value.map { model -> MovieViewModel in
+                    let savedMovieIds = self.moviesUseCase.favoriteItems
+                    let isSaved = savedMovieIds.contains(model.id)
+                    return MovieViewModel(
+                        id: model.id,
+                        imageUrl: model.imageUrl,
+                        isFavorite: isSaved
+                    )
                 }
+                self.data = viewModels.filter { $0.isFavorite == true }
+                self.delegate?.reloadData()
             }
         }
     }
