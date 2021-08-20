fca7e6cd1a91e53bd7a3a5efe42a97bbde957b36
srcxp <antoniomarkotic1@gmail.com>
2021-08-20 15:36:17 +0200

    Lost files retrieved

---
+++ MovieApp/MovieApp/App/Features/Movies/Components/DetailsComponents/CastView/CollectionViewCell/CastCollectionViewCell+Design.swift
@@ -0,0 +1,37 @@
+import UIKit
+import SnapKit
+
+extension CastCollectionViewCell: DesignProtocol {
+    
+    func buildViews() {
+        createViews()
+        styleViews()
+        defineLayoutForViews()
+    }
+    
+    func createViews() {
+        name = UILabel(with: .bold(size: 14))
+        addSubview(name)
+        
+        role = UILabel(with: .proximaMedium)
+        addSubview(role)
+    }
+    
+    func styleViews() {
+        name.textColor = .appBlack
+        
+        role.textColor = .appBlack
+    }
+    
+    func defineLayoutForViews() {
+        name.snp.makeConstraints {
+            $0.leading.top.trailing.equalToSuperview()
+        }
+        
+        role.snp.makeConstraints {
+            $0.top.equalTo(name.snp.bottom).offset(defaultOffset)
+            $0.leading.bottom.trailing.equalToSuperview()
+        }
+    }
+
+}
---
+++ MovieApp/MovieApp/App/Features/Movies/Components/DetailsComponents/CastView/CollectionViewCell/CastCollectionViewCell.swift
@@ -0,0 +1,27 @@
+import UIKit
+
+class CastCollectionViewCell: UICollectionViewCell {
+    
+    let defaultOffset = 5
+    
+    static let reuseIdentifier = String(describing: CastCollectionViewCell.self)
+    
+    var name: UILabel!
+    var role: UILabel!
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        buildViews()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setData(with model: CastViewModel) {
+        name.text = model.name
+        role.text = model.role
+    }
+    
+}
