9fd22f2057a9370a94a295b231c7fc76a0a00aa5
srcxp <antoniomarkotic1@gmail.com>
2021-09-15 14:04:51 +0200

    Reachability implemented

--- MovieApp/MovieApp/App/Localization/LocalizableStrings.swift
+++ MovieApp/MovieApp/App/Localization/LocalizableStrings.swift
@@ -17,5 +17,6 @@ enum LocalizableStrings: String {
     case acting = "Acting"
     case noReview = "No reviews found"
     case tryAgain = "\nPlease try again later"
+    case noInternet = "No internet connection, please try again later"
 
 }
